cb62ac14f81cbaa20d66d071aa02cee39ad27026
Makcim <94489688+Makcim1997@users.noreply.github.com>
2022-11-02 11:48:47 +0300

    refactor: refactoring code

--- portfolio/index.js
+++ portfolio/index.js
@@ -75,9 +75,12 @@ function activeBtn(event) {
     btnsLang.forEach(elem => elem.classList.remove('active-lang'))
   }
 
-  event.target.classList.contains('lang-ru') ? setTransleteRu() : setTransleteEn();
+  if (event.target.classList.contains('lang-ru')) {
+    setTransleteRu()
+  } else if ( event.target.classList.contains('lang-en')) {
+    setTransleteEn()
+  }
   
-
   if (event.target.classList.contains('lang')) {
     event.target.classList.add('active-lang')
   }
@@ -166,138 +169,3 @@ function toggleTopic() {
     body.classList.remove('change-body');
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*====== change images and button colors ======*/
-
-
-// const portfolioImage = document.querySelectorAll('.photo')
-// const portfolioBtns = document.querySelector('.portfolio-items')
-// const portfolioBtnsColor = document.querySelectorAll('.portfolio-button')
-
-// portfolioBtns.addEventListener('click', changeImage)
-
-// portfolioBtnsColor[3].classList.add('portfoliobtn-active')
-
-
-// function changeImage(event) {
-//   event.target.classList.contains('.portfolio-btn')
-//   portfolioImage.forEach((img, index) => img.src = `../portfolio/assets/images/seasons/${event.target.dataset.seasons}/${index + 1}.jpg`)
-//   portfolioBtnsColor.forEach((colors) => colors.classList.remove('portfoliobtn-active'))
-//   event.target.classList.toggle('portfoliobtn-active')
-  
-     
-// }
-
-/*====== change images and button colors ======*/
-
-/*====== change theme ======*/
-
-// const buttonTheme = document.querySelector('.theme')
-// const changeBtnTheme = document.querySelectorAll('.theme-button')
-// const themeColor = document.querySelector('.theme-dark')
-
-// changeBtnTheme[0].classList.add('sun')
-
-// buttonTheme.addEventListener('click', (items) => {
-//     changeBtnTheme.forEach((themes) => themes.classList.remove('sun'))
-//     items.target.classList.toggle('sun')
-//     themeColor.classList.toggle('active-theme')
-    
-// })
-
-// // document.querySelector('.theme').addEventListener('click', (item) => {
-// //   if (item.target.nodeName === 'IMG') { 
-// //     document.documentElement.classList.remove('dark', 'light')
-// //     document.documentElement.classList.toggle(item.target.value)
-// //   }
-// // })
-
-
-//   colorBurger.addEventListener('mouseenter', (span) => {
-//     span.target.classList === 'menu-btn'
-//     colorSpan.forEach((colors) => colors.classList.toggle('color'))
-//   })
-
-//   colorBurger.addEventListener('mouseleave', (span) => {
-//     span.target.classList === 'menu-btn'
-//     colorSpan.forEach((colors) => colors.classList.toggle('color'))
-//   })
-
-//   const colorTheme = document.querySelector('.theme')
-//   const colorThemes = document.querySelectorAll('.theme-button')
-
-//   colorTheme.addEventListener('mouseenter', (theme) => {
-//     theme.target.classList === 'theme-button'
-//     colorThemes.forEach((themes) => themes.classList.toggle('color'))
-//   })
-
-//   colorTheme.addEventListener('mouseleave', (theme) => {
-//     theme.target.classList === 'theme-button'
-//     colorThemes.forEach((themes) => themes.classList.toggle('color'))
-//   })
-
-  /*====== menu-burger ======*/
-
-
-
-
-
-
-
-
-
-
-
-
